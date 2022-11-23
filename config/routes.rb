9f233af45c133157c647207be45919b5f92a73b2
guillaume drevo <guillaume.drevo@gmail.com>
2022-11-23 14:21:30 +0100

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,32 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/restaurants", to: "restaurants#index"
+
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+
+  get "/restaurants/:id", to: "restaurants#show"
+
+  post "restaurants", to: "restaurants#create"
+
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
+
+  # get "/reviews/new", to: "reviews#new", as: :new_review
+
+  # get "/reviews/:id", to: "reviews#show"
+
+  # post "/reviews", to: "reviews#create"
 end
+
+
+
+# get "/tasks", to: "tasks#index"
+# get "/tasks/new", to: "tasks#new", as: :new_task
+# post "tasks", to: "tasks#create"
+# get "/tasks/:id", to: "tasks#show", as: :task
+# get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+# patch "tasks/:id", to: "tasks#update"
+# delete "tasks/:id", to: "tasks#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "le Blev-Hir", address: "20 rue Bruat, Brest, France", phone_number: "0298056135", category: "french")
+
+Restaurant.create(name: "Chinese restaurant", address: "35 place de Bretagne, Rennes, France", phone_number: "0299656139", category: "chinese")
+
+Restaurant.create(name: "Trattoria del mezzogiorno", address: "35 piazza Trilussa, Roma, Italia", phone_number: "01785613256", category: "italian")
+
+Restaurant.create(name: "Friterie Belge", address: "40 rue d'Anvers, Bruxelles, Belgique", phone_number: "0956483265", category: "belgian")
+
+Restaurant.create(name: "La cocotte dorée", address: "25 place d'Espagne, Lyon, France", phone_number: "0458796321", category: "french")
